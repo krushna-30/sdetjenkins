6935a5611919b667dcf7c11d5fd1b680c32c0a53
neojarvis <techadmin@iamneo.ai>
2025-03-27 13:39:40 +0000

    my initial code

--- jenkinsproject/src/test/java/com/lti/SampleTest.java
+++ jenkinsproject/src/test/java/com/lti/SampleTest.java
@@ -1,5 +1,18 @@
 package com.lti;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class SampleTest {
     
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("*****************Testcase Passed******************");
+    }
 }
